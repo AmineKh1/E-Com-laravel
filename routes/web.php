2214e69d32453f9f195d22b3beb16c75b7346e9e
amine khouya <amine.khouya1999@gmail.com>
2021-07-30 17:58:28 +0200

    e-comm

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ Route::get('/logout', function () {
     Session::forget('user');
     return redirect('login');
 });
+route::view('/register','register');
+Route::post("/register",[UserController::class,'register']);
+
 Route::post("/login",[UserController::class,'login']);
 Route::get("/",[ProductController::class,'index']);
 Route::get("/detail/{id}",[ProductController::class,'detail']);
